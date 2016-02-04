2cb9a56a4a8f20bd43940f07ac39364392b420ca
LittleBuster <DenisovS21@gmail.com>
2016-02-04 11:32:28 +0000

    modify piio

--- piio.h
+++ piio.h
@@ -23,70 +23,73 @@ enum io_errs {
 };
 
 
-/**
- * IO library initialization
- *
- * returns 0 if succeful
- * returns ERR_DEVMEM if fail init (need root)
- * returns ERR_MMAP if fail reading mmap
- */
-int pi_init(void);
+class GPIO
+{
+public:
+    /**
+     * IO library initialization
+     *
+     * returns 0 if succeful
+     * returns ERR_DEVMEM if fail init (need root)
+     * returns ERR_MMAP if fail reading mmap
+     */
+    static int init(void);
 
-/**
- * Set port in input mode
- * @gpio: port number
- */
-void pi_set_input(const int gpio);
+    /**
+     * Set port in input mode
+     * @gpio: port number
+     */
+    void setInput(const int gpio);
 
-/**
- * Set port in input mode
- * @gpio: port number
- */
-void pi_set_output(const int gpio);
+    /**
+     * Set port in input mode
+     * @gpio: port number
+     */
+    void setOutput(const int gpio);
 
-/**
- * Set port in high level
- * @gpio: port number
- */
-void pi_set_high(const int gpio);
+    /**
+     * Set port in high level
+     * @gpio: port number
+     */
+    void setHigh(const int gpio);
 
-/**
- * Set port in low level
- * @gpio: port number
- */
-void pi_set_low(const int gpio);
+    /**
+     * Set port in low level
+     * @gpio: port number
+     */
+    void setLow(const int gpio);
 
-/**
- * Reading data from port
- * @gpio: port number
- *
- * returns port value
- */
-uint32_t pi_read(const int gpio);
+    /**
+     * Reading data from port
+     * @gpio: port number
+     *
+     * returns port value
+     */
+    uint32_t read(const int gpio);
 
-/**
- * Busy wait delay for most accurate timing, but high CPU usage.
- * Only use this for short periods of time (a few hundred milliseconds at most)!
- * @millis: wait milliseconds
- */
-void pi_wait_millis(uint32_t millis);
+    /**
+     * Busy wait delay for most accurate timing, but high CPU usage.
+     * Only use this for short periods of time (a few hundred milliseconds at most)!
+     * @millis: wait milliseconds
+     */
+    void waitMillis(uint32_t millis);
 
-/**
- * General delay that sleeps so CPU usage is low,
- * but accuracy is potentially bad.
- * @millis: sleep milliseconds
- */
-void pi_sleep_millis(uint32_t millis);
+    /**
+     * General delay that sleeps so CPU usage is low,
+     * but accuracy is potentially bad.
+     * @millis: sleep milliseconds
+     */
+    void sleepMillis(uint32_t millis);
 
-/*
- * Increase scheduling priority and algorithm to try to get 'real time' results.
- */
-void pi_max_priority(void);
+    /*
+     * Increase scheduling priority and algorithm to try to get 'real time' results.
+     */
+    void maxPriority(void);
 
-/*
- * Drop scheduling priority back to normal/default.
- */
-void pi_default_priority(void);
-
+    /*
+     * Drop scheduling priority back to normal/default.
+     */
+    void defaultPriority(void);
+};
 
 #endif
